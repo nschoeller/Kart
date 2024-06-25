3b6235d3356deca92bf2c004420ea16b839b935f
Nicholas Schoeller <nschoeller2@gmail.com>
2024-06-25 11:48:41 -0400

    initial file

---
+++ app.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import dash
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+if __name__ == '__main__':
+  print("Hello world")
+
